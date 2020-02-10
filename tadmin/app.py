d82536efca8a7eebe81ef4d5cec8b0b407d12a15
Timofeev Alexander <timofeev162@gmail.com>
2020-02-11 00:29:40 +0300

    ISSUE-11 Добавлена фабрика Flask-приложения
    Изменено тело функции create_app
    Добавлена инициализация приложения

--- tadmin/app.py
+++ tadmin/app.py
@@ -4,9 +4,8 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    return app
+    @app.route('/')
+    def fac_app():
+        return 'OK'
 
-
-@app.route('/')
-def app():
-    return 'OK'
+    return fac_app
